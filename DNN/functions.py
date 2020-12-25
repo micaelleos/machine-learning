02e1ee9d40ce37e8b285ac71856c1711c5dd8fef
micaelleos <micaelle.oliveira10@hotmail.com>
2020-12-25 13:04:38 -0300

    estrutura

--- DNN/functions.py
+++ DNN/functions.py
@@ -13,40 +13,54 @@ def inicializa(netdim):
 def propForward(A,variaveis):
 
     L = len(variaveis)/2
-
+    variaveis2={}
     for i in range(0,L-1):
         W=variaveis['W'+i]
         b=variaveis['b'+i]
         Z=np.dot(W,A) + b
         A=np.maximum(Z) # Relu
+        variaveis2['Z'+i]=Z
+        variaveis2['A'+i]=A
 
     W=variaveis['W'+L]
     b=variaveis['b'+L]
     Z=np.dot(W,A) + b
     AL=1/(1 + np.exp(-Z)) # sigmoid
-    
-    return AL
+    variaveis2['Z'+L]=Z
+    variaveis2['A'+L]=AL
+    return AL, variaveis2
 
-def propBackward(AL,Y,variaveis):
+def propBackward(AL,Y,variaveis,var2):
     L = len(variaveis)/2
     m=AL.shape[0]
+    gradientes={}
 
-    dA=(1/m)*np.sum(Y/AL - (1-Y)/(1-AL))
-    dZ= np.dot(dA, (1-np.sqrt(AL)))
-    dW= dZ * 
-    db=
+    A=var2['A'+ srt(range(0,L+1))]
+    Z=var2['Z'+ srt(range(0,L+1))]
+    W=variaveis['W'+srt(range(0,L+1))]
     
-    for i in range(0,L-1):
-        dA=
-        dZ=
-        dW=
-        db=
+    dA=(1/m)*np.sum(Y/AL - (1-Y)/(1-AL))
+    dZ=dA*(1-np.sqrt(AL)) #derivada da camada com sigmoid
+    
+    for i in range(L-1,0,-1):
+        dW=dZ*A[i-1]
+        db=dZ
+        dA=dZ*W[i]
+        dZ=dA*np.heaviside(Z[i-1]) # derivada da camada com Relu
+        gradientes['dW'+i]=dW
+        gradientes['db'+i]=db
+    
+    return gradientes
 
-    return
 
-
-def atualizaparam(gradientes):
-
+def atualizaparam(alfa,gradientes,variaveis):
+    var={}
+    L=len(gradientes)/2
+    for i in range(0,L+1):
+        var['W'+i]=variaveis['W'+i]+alfa*gradientes['dW'+i]
+        var['b'+i]=variaveis['b'+i]+alfa*gradientes['db'+i]
+    return var
 
 
 def custo(AL,Y):
+    return J=(1/len(Y))*np.sum(Y*log(AL)-(1-Y)*log(1-AL))
--- DNN/main.py
+++ DNN/main.py
@@ -5,16 +5,18 @@ from functions import *
 X=
 Y=
 
-netdim = []
+netdim = [] #dimensões das camadas
 variaveis = inicializa(netdim)
-AL=X
+A=X
+alfa=0.1 # taxa de aprendizado
+n=1000 # número de iterações
 
 np.random
 
-for i in range(1,inte)
-    AL = propForward(A,variaveis)#calculo do modelo
-    gradientes = propBackward(AL,Y,variaveis)#backpropagations
-    variaveis = atualizaparam(gradientes)
+for i in range(1,n)
+    AL,var2 = propForward(A,variaveis)#calculo do modelo
+    gradientes = propBackward(AL,Y,variaveis,var2)#backpropagations
+    variaveis = atualizaparam(alfa,gradientes,variaveis)
     A=AL
     J = custo(AL,Y)#custo
     #printar J
